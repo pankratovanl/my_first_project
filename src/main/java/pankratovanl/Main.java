9a79a1fc4f60b95d919071052002a19b69e79462
pankratovanl <nkvcxmfqqffi@mail.ru>
2025-01-12 22:22:14 +0500

    added getMessage() method

--- src/main/java/pankratovanl/Main.java
+++ src/main/java/pankratovanl/Main.java
@@ -2,6 +2,7 @@ package pankratovanl;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Promed!");
+
+        System.out.println("Hello, Inspector!");
     }
 }
